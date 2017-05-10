9654f10430fb98999bed668322add94fe57c2a15
Vasilis Ventirozos <vasilis@Lust.local>
2017-05-10 03:28:46 +0300

    too many to tell, refactored statz.py added graphs

--- statz.py
+++ statz.py
@@ -58,56 +58,65 @@ def schema_init():
             create table if not exists statz.table_activity as select now()::timestamp without time zone as snap_date,* from pg_stat_user_tables limit 0;
             create table if not exists statz.database_activity as select now()::timestamp without time zone as snap_date,* from pg_stat_database limit 0;
             create table if not exists statz.bgwriter_activity as select now()::timestamp without time zone as snap_date,* from pg_stat_bgwriter limit 0;
-            CREATE TABLE If not exists statz.database_activity_agg (
-                snap_date timestamp without time zone,
-                "interval" interval,
-                step interval,
-                datname name,
-                commits bigint,
-                rows_returned bigint,
-                rows_fetched bigint,
-                rows_inserted bigint,
-                rows_updated bigint,
-                rows_deleted bigint,
-                blocks_read bigint,
-                blocks_hit_cached bigint,
-                commits_per_sec bigint,
-                rows_returned_per_sec bigint,
-                rows_fetched_per_sec bigint,
-                rows_inserted_per_sec bigint,
-                rows_updated_per_sec bigint,
-                rows_deleted_per_sec bigint,
-                blocks_read_per_sec bigint,
-                blocks_hit_cached_per_sec bigint,
-                txn_per_sec bigint,
-                cache_hit_ratio numeric
-            );
-            CREATE TABLE If not exists statz.table_activity_agg (
-                snap_date timestamp without time zone,
-                "interval" interval,
-                step interval,
-                table_name text,
-                seq_scans bigint,
-                seq_rows_read bigint,
-                index_scans bigint,
-                index_rows_fetched bigint,
-                rows_inserted bigint,
-                rows_updated bigint,
-                rows_deleted bigint,
-                rows_hot_updated bigint,
-                live_row_count bigint,
-                n_dead_tup bigint,
-                seq_scans_per_sec bigint,
-                seq_rows_read_per_sec bigint,
-                index_scans_per_sec bigint,
-                index_rows_fetched_per_sec bigint,
-                rows_inserted_per_sec bigint,
-                rows_updated_per_sec bigint,
-                rows_deleted_per_sec bigint,
-                rows_hot_updated_per_sec bigint,
-                live_row_count_per_sec bigint,
-                n_dead_tup_per_sec bigint
-                );
+
+            CREATE view statz.database_activity_agg AS select
+            snap_date,
+            snap_date-LAG(snap_date, 1, snap_date) OVER (ORDER BY snap_date) AS interval,
+            snap_date - (select min (snap_date) from statz.database_activity) as step,
+            datname,
+            xact_commit - LAG(xact_commit, 1, xact_commit) OVER (ORDER BY snap_date) as commits,
+            xact_rollback - LAG(xact_rollback, 1, xact_rollback) OVER (ORDER BY snap_date) as rollbacks,
+            tup_returned - LAG(tup_returned, 1, tup_returned) OVER (ORDER BY snap_date) as rows_returned,
+            tup_fetched - LAG(tup_fetched, 1, tup_fetched) OVER (ORDER BY snap_date)  as rows_fetched,
+            tup_inserted - LAG(tup_inserted, 1, tup_inserted) OVER (ORDER BY snap_date) as rows_inserted,
+            tup_updated - LAG(tup_updated, 1, tup_updated) OVER (ORDER BY snap_date) as rows_updated,
+            tup_deleted - LAG(tup_deleted, 1, tup_deleted) OVER (ORDER BY snap_date) as rows_deleted,
+            blks_read - LAG(blks_read, 1, blks_read) OVER (ORDER BY snap_date) as blocks_read,
+            blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date) as blocks_hit_cached,
+            (xact_commit - LAG(xact_commit, 1, xact_commit) OVER (ORDER BY snap_date) ) / {0}::int as commits_per_sec,
+            (xact_rollback - LAG(xact_rollback, 1, xact_rollback) OVER (ORDER BY snap_date) ) / {0}::int as rollbacks_per_sec,
+            (tup_returned - LAG(tup_returned, 1, tup_returned) OVER (ORDER BY snap_date)) / {0}::int as rows_returned_per_sec,
+            (tup_fetched - LAG(tup_fetched, 1, tup_fetched) OVER (ORDER BY snap_date)) / {0}::int as rows_fetched_per_sec,
+            (tup_inserted - LAG(tup_inserted, 1, tup_inserted) OVER (ORDER BY snap_date)) / {0}::int as rows_inserted_per_sec,
+            (tup_updated - LAG(tup_updated, 1, tup_updated) OVER (ORDER BY snap_date)) / {0}::int as rows_updated_per_sec,
+            (tup_deleted - LAG(tup_deleted, 1, tup_deleted) OVER (ORDER BY snap_date)) / {0}::int as rows_deleted_per_sec,
+            (blks_read - LAG(blks_read, 1, blks_read) OVER (ORDER BY snap_date)) / {0}::int as blocks_read_per_sec,
+            (blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date)) / {0}::int as blocks_hit_cached_per_sec,
+            (( xact_commit + xact_rollback ) - (LAG(xact_commit, 1, xact_commit) OVER (ORDER BY snap_date) +
+            LAG(xact_rollback, 1, xact_rollback) OVER (ORDER BY snap_date))) / {0}::int as txn_per_sec,
+            round(CAST (((blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date) )::real /
+            ((blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date)) +
+            (blks_read - LAG(blks_read, 1, blks_read) OVER (ORDER BY snap_date) )+0.001)) * 100 as numeric ),4) as cache_hit_ratio
+            FROM statz.database_activity order by snap_date;
+
+            CREATE VIEW statz.table_activity_agg AS select *
+                from (select
+                snap_date,
+                snap_date-LAG(snap_date, 1, snap_date) OVER (ORDER BY relid,snap_date) AS interval,
+                snap_date - (select min (snap_date) from statz.table_activity) as step,
+                schemaname||'.'||relname as table_name,
+                coalesce( seq_scan - LAG(seq_scan, 1, seq_scan) OVER (ORDER BY relid,snap_date) ,'0') as seq_scans,
+            	coalesce( seq_tup_read - LAG(seq_tup_read, 1, seq_tup_read) OVER (ORDER BY relid,snap_date) ,'0') as seq_rows_read ,
+            	coalesce( idx_scan - LAG(idx_scan, 1, idx_scan) OVER (ORDER BY relid,snap_date) ,'0') as index_scans,
+            	coalesce( idx_tup_fetch - LAG(idx_tup_fetch, 1, idx_tup_fetch) OVER (ORDER BY relid,snap_date) ,'0') as index_rows_fetched,
+            	coalesce( n_tup_ins - LAG(n_tup_ins, 1, n_tup_ins) OVER (ORDER BY relid,snap_date) ,'0') as rows_inserted,
+            	coalesce( n_tup_upd - LAG(n_tup_upd, 1, n_tup_upd) OVER (ORDER BY relid,snap_date) ,'0') as rows_updated,
+            	coalesce( n_tup_del - LAG(n_tup_del, 1, n_tup_del) OVER (ORDER BY relid,snap_date) ,'0') as rows_deleted,
+            	coalesce( n_tup_hot_upd - LAG(n_tup_hot_upd, 1, n_tup_hot_upd) OVER (ORDER BY relid,snap_date) ,'0') as rows_hot_updated,
+            	coalesce( n_live_tup - LAG(n_live_tup, 1, n_live_tup) OVER (ORDER BY relid,snap_date) ,'0') as live_row_count,
+            	coalesce( n_dead_tup - LAG(n_dead_tup, 1, n_dead_tup) OVER (ORDER BY relid,snap_date) ,'0') as n_dead_tup,
+            	coalesce( seq_scan - LAG(seq_scan, 1, seq_scan) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as seq_scans_per_sec,
+            	coalesce( seq_tup_read - LAG(seq_tup_read, 1, seq_tup_read) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as seq_rows_read_per_sec ,
+            	coalesce( idx_scan - LAG(idx_scan, 1, idx_scan) OVER (ORDER BY relid,snap_date) ,'0') as index_scans_per_sec,
+            	coalesce( idx_tup_fetch - LAG(idx_tup_fetch, 1, idx_tup_fetch) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as index_rows_fetched_per_sec,
+            	coalesce( n_tup_ins - LAG(n_tup_ins, 1, n_tup_ins) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as rows_inserted_per_sec,
+            	coalesce( n_tup_upd - LAG(n_tup_upd, 1, n_tup_upd) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as rows_updated_per_sec,
+            	coalesce( n_tup_del - LAG(n_tup_del, 1, n_tup_del) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as rows_deleted_per_sec,
+            	coalesce( n_tup_hot_upd - LAG(n_tup_hot_upd, 1, n_tup_hot_upd) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as rows_hot_updated_per_sec,
+            	coalesce( n_live_tup - LAG(n_live_tup, 1, n_live_tup) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as live_row_count_per_sec,
+            	coalesce( n_dead_tup - LAG(n_dead_tup, 1, n_dead_tup) OVER (ORDER BY relid,snap_date) ,'0') / {0}::int as n_dead_tup_per_sec
+                from statz.table_activity ) as foo where foo.interval >'0s';
+
             create view statz.table_stats_per_sec as
                     select snap_date,
                     step,
@@ -119,11 +128,14 @@ def schema_init():
                     rows_inserted_per_sec,
                     rows_updated_per_sec,
                     rows_deleted_per_sec,
-                    rows_hot_updated_per_sec from statz.table_activity_agg;
+                    rows_hot_updated_per_sec
+                    from statz.table_activity_agg
+                    order by snap_date;
             create view statz.db_stats_per_sec as
                     select snap_date,
                     step ,
                     commits_per_sec ,
+                    rollbacks_per_sec,
                     rows_returned_per_sec ,
                     rows_fetched_per_sec ,
                     rows_inserted_per_sec ,
@@ -131,15 +143,16 @@ def schema_init():
                     rows_deleted_per_sec ,
                     blocks_read_per_sec ,
                     blocks_hit_cached_per_sec ,
-                    txn_per_sec from statz.database_activity_agg ;
+                    txn_per_sec
+                    from statz.database_activity_agg
+                    order by snap_date ;
 
-            commit ;"""
+            commit ;""".format(finterval)
     cursor = conn.cursor()
     cursor.execute(init_statz)
 
 
 def main_task():
-    print "gathering statz.."
     all_statz_gather_sql = """
             insert into statz.backend_activity select now()::timestamp(0),* from pg_stat_activity;
             insert into statz.lock_activity select now()::timestamp(0),* from pg_locks;
@@ -151,81 +164,15 @@ def main_task():
     cursor = conn.cursor()
     cursor.execute(all_statz_gather_sql)
 
-def db_statz():
-    stat_db_sql = """
-        insert into statz.database_activity_agg select
-        snap_date,
-        snap_date-LAG(snap_date, 1, snap_date) OVER (ORDER BY snap_date) AS interval,
-        snap_date - (select min (snap_date) from statz.database_activity) as step,
-        datname,
-        xact_commit - LAG(xact_commit, 1, xact_commit) OVER (ORDER BY snap_date) as commits,
-        tup_returned - LAG(tup_returned, 1, tup_returned) OVER (ORDER BY snap_date) as rows_returned,
-        tup_fetched - LAG(tup_fetched, 1, tup_fetched) OVER (ORDER BY snap_date)  as rows_fetched,
-        tup_inserted - LAG(tup_inserted, 1, tup_inserted) OVER (ORDER BY snap_date) as rows_inserted,
-        tup_updated - LAG(tup_updated, 1, tup_updated) OVER (ORDER BY snap_date) as rows_updated,
-        tup_deleted - LAG(tup_deleted, 1, tup_deleted) OVER (ORDER BY snap_date) as rows_deleted,
-        blks_read - LAG(blks_read, 1, blks_read) OVER (ORDER BY snap_date) as blocks_read,
-        blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date) as blocks_hit_cached,
-        (xact_commit - LAG(xact_commit, 1, xact_commit) OVER (ORDER BY snap_date) ) / {0} as commits_per_sec,
-        (tup_returned - LAG(tup_returned, 1, tup_returned) OVER (ORDER BY snap_date)) / {0} as rows_returned_per_sec,
-        (tup_fetched - LAG(tup_fetched, 1, tup_fetched) OVER (ORDER BY snap_date)) / {0} as rows_fetched_per_sec,
-        (tup_inserted - LAG(tup_inserted, 1, tup_inserted) OVER (ORDER BY snap_date)) / {0} as rows_inserted_per_sec,
-        (tup_updated - LAG(tup_updated, 1, tup_updated) OVER (ORDER BY snap_date)) / {0} as rows_updated_per_sec,
-        (tup_deleted - LAG(tup_deleted, 1, tup_deleted) OVER (ORDER BY snap_date)) / {0} as rows_deleted_per_sec,
-        (blks_read - LAG(blks_read, 1, blks_read) OVER (ORDER BY snap_date)) / {0} as blocks_read_per_sec,
-        (blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date)) / {0} as blocks_hit_cached_per_sec,
-        (( xact_commit + xact_rollback ) - (LAG(xact_commit, 1, xact_commit) OVER (ORDER BY snap_date) +
-        LAG(xact_rollback, 1, xact_rollback) OVER (ORDER BY snap_date))) / {0} as txn_per_sec,
-        round(CAST (((blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date) )::real /
-        ((blks_hit - LAG(blks_hit, 1, blks_hit) OVER (ORDER BY snap_date)) +
-        (blks_read - LAG(blks_read, 1, blks_read) OVER (ORDER BY snap_date) )+0.001)) * 100 as numeric ),4) as cache_hit_ratio
-        FROM statz.database_activity where snap_date in (select snap_date from statz.database_activity order by snap_date desc limit 2) limit 1 offset 1 ;
-        commit;
-        """.format(finterval)
-    cursor = conn.cursor()
-    cursor.execute(stat_db_sql)
-
-def table_statz():
-    table_statz_sql = """
-    insert into statz.table_activity_agg select *
-        from (select
-        snap_date,
-        snap_date-LAG(snap_date, 1, snap_date) OVER (ORDER BY relid,snap_date) AS interval,
-        snap_date - (select min (snap_date) from statz.table_activity) as step,
-        schemaname||'.'||relname as table_name,
-        coalesce( seq_scan - LAG(seq_scan, 1, seq_scan) OVER (ORDER BY relid,snap_date) ,'0') as seq_scans,
-    	coalesce( seq_tup_read - LAG(seq_tup_read, 1, seq_tup_read) OVER (ORDER BY relid,snap_date) ,'0') as seq_rows_read ,
-    	coalesce( idx_scan - LAG(idx_scan, 1, idx_scan) OVER (ORDER BY relid,snap_date) ,'0') as index_scans,
-    	coalesce( idx_tup_fetch - LAG(idx_tup_fetch, 1, idx_tup_fetch) OVER (ORDER BY relid,snap_date) ,'0') as index_rows_fetched,
-    	coalesce( n_tup_ins - LAG(n_tup_ins, 1, n_tup_ins) OVER (ORDER BY relid,snap_date) ,'0') as rows_inserted,
-    	coalesce( n_tup_upd - LAG(n_tup_upd, 1, n_tup_upd) OVER (ORDER BY relid,snap_date) ,'0') as rows_updated,
-    	coalesce( n_tup_del - LAG(n_tup_del, 1, n_tup_del) OVER (ORDER BY relid,snap_date) ,'0') as rows_deleted,
-    	coalesce( n_tup_hot_upd - LAG(n_tup_hot_upd, 1, n_tup_hot_upd) OVER (ORDER BY relid,snap_date) ,'0') as rows_hot_updated,
-    	coalesce( n_live_tup - LAG(n_live_tup, 1, n_live_tup) OVER (ORDER BY relid,snap_date) ,'0') as live_row_count,
-    	coalesce( n_dead_tup - LAG(n_dead_tup, 1, n_dead_tup) OVER (ORDER BY relid,snap_date) ,'0') as n_dead_tup,
-    	coalesce( seq_scan - LAG(seq_scan, 1, seq_scan) OVER (ORDER BY relid,snap_date) ,'0') / {0} as seq_scans_per_sec,
-    	coalesce( seq_tup_read - LAG(seq_tup_read, 1, seq_tup_read) OVER (ORDER BY relid,snap_date) ,'0') / {0} as seq_rows_read_per_sec ,
-    	coalesce( idx_scan - LAG(idx_scan, 1, idx_scan) OVER (ORDER BY relid,snap_date) ,'0') as index_scans_per_sec,
-    	coalesce( idx_tup_fetch - LAG(idx_tup_fetch, 1, idx_tup_fetch) OVER (ORDER BY relid,snap_date) ,'0') / {0} as index_rows_fetched_per_sec,
-    	coalesce( n_tup_ins - LAG(n_tup_ins, 1, n_tup_ins) OVER (ORDER BY relid,snap_date) ,'0') / {0} as rows_inserted_per_sec,
-    	coalesce( n_tup_upd - LAG(n_tup_upd, 1, n_tup_upd) OVER (ORDER BY relid,snap_date) ,'0') / {0} as rows_updated_per_sec,
-    	coalesce( n_tup_del - LAG(n_tup_del, 1, n_tup_del) OVER (ORDER BY relid,snap_date) ,'0') / {0} as rows_deleted_per_sec,
-    	coalesce( n_tup_hot_upd - LAG(n_tup_hot_upd, 1, n_tup_hot_upd) OVER (ORDER BY relid,snap_date) ,'0') / {0} as rows_hot_updated_per_sec,
-    	coalesce( n_live_tup - LAG(n_live_tup, 1, n_live_tup) OVER (ORDER BY relid,snap_date) ,'0') / {0} as live_row_count_per_sec,
-    	coalesce( n_dead_tup - LAG(n_dead_tup, 1, n_dead_tup) OVER (ORDER BY relid,snap_date) ,'0') / {0} as n_dead_tup_per_sec
-        from statz.table_activity ) as foo where foo.interval >'0s';
-    """.format(finterval)
-    cursor = conn.cursor()
-    cursor.execute(table_statz_sql)
 
 def run():
     conn_init()
     schema_init()
+    print "Gathering statz, standby.."
     start_time = time.time()
     while (time.time() - start_time) < fduration:
         main_task()
         time.sleep(finterval)
-        db_statz()
-        table_statz()
+#        print "work is being done.."
 if __name__ == "__main__":
     run()
---
+++ statz_graphs.py
@@ -0,0 +1,228 @@
+#!/usr/local/bin/python
+import sys
+import psycopg2
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
+import seaborn as sns
+sns.set_style("whitegrid")
+
+parser = argparse.ArgumentParser(description="Description goes here")
+parser.add_argument('-c','--connection', default="dbname=postgres", help="""Connection string for use by psycopg. Defaults to "dbname=postgres" (local socket connecting to postgres database). dbname parameter in connection string is required.""")
+parser.add_argument('-t', dest='table_to_graph', nargs='*', help="Table you want to get graphs for.")
+parser.add_argument('-d', dest='do_db_graph', action='store_true', help="export db graphs")
+parser.add_argument('--debug', action="store_true", help="Show additional debugging output")
+args = parser.parse_args()
+
+print(args)
+
+
+def conn_init():
+    global dbname
+    dbname_found = False
+    for c in args.connection.split(" "):
+        if args.debug:
+            print("connection paramter: " + str(c))
+        if c.find("dbname=") != -1:
+            dbname = c.split("=")[1]
+            dbname_found = True
+            break
+    if not dbname_found:
+        print("Missing dbname parameter in database connection string")
+        sys.exit(2)
+
+    if args.debug:
+        print "Connecting to database   -> %s" % (dbname)
+
+    conn = psycopg2.connect(args.connection)
+    return conn
+
+
+#def conn_init():
+#    try:
+#        conn = psycopg2.connect(database='monkey', host='localhost', user='vasilis')
+#    except:
+#        print "Unable to connect to the database"
+#        exit(2)
+#    return conn
+
+def get_tablestatz(table_name):
+    conn = conn_init()
+    cur = conn.cursor()
+    query = """SELECT snap_date,
+                    step,
+                    table_name,
+                    seq_scans_per_sec,
+                    seq_rows_read_per_sec,
+                    index_scans_per_sec,
+                    index_rows_fetched_per_sec,
+                    rows_inserted_per_sec,
+                    rows_updated_per_sec,
+                    rows_deleted_per_sec,
+                    rows_hot_updated_per_sec
+                from statz.table_stats_per_sec
+                where table_name like '%{0}'; """.format(table_name)
+    cur.execute(query)
+    tabledata = cur.fetchall()
+    conn.close()
+    return tabledata
+
+def get_dbstatz():
+    conn = conn_init()
+    cur = conn.cursor()
+    query = """SELECT snap_date,
+                commits_per_sec,
+                rollbacks_per_sec,
+                rows_returned_per_sec,
+                rows_fetched_per_sec,
+                rows_inserted_per_sec,
+                rows_updated_per_sec,
+                rows_deleted_per_sec,
+                blocks_read_per_sec,
+                blocks_hit_cached_per_sec,
+                txn_per_sec
+                FROM statz.db_stats_per_sec;"""
+    cur.execute(query)
+    dbdata = cur.fetchall()
+    conn.close()
+    return dbdata
+
+def plot_dbstatz():
+    dbdata = get_dbstatz()
+    values = zip(*dbdata)
+#
+    time = values[0]
+    commits_per_sec = values[1]
+    rollbacks_per_sec = values[2]
+    rows_returned_per_sec = values[3]
+    rows_fetched_per_sec = values[4]
+    rows_inserted_per_sec = values[5]
+    rows_updated_per_sec = values[6]
+    rows_deleted_per_sec = values[7]
+    blocks_read_per_sec = values[8]
+    blocks_hit_cached_per_sec = values[9]
+    txn_per_sec = values[10]
+#
+    plt.suptitle('Database statistics per sec', fontsize=28, fontweight='bold')
+    plt.subplot(3, 4, 1)
+    plt.plot(time,commits_per_sec, label='Commits / sec' )
+    plt.title('Commits / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 2)
+    plt.plot(time,rollbacks_per_sec, label='Rollbacks / sec' )
+    plt.title('Rollbacks / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 3)
+    plt.plot(time,rows_returned_per_sec, label='Rows returned / sec' )
+    plt.title('Rows returned / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 4)
+    plt.plot(time,rows_fetched_per_sec, label='Rows fetched / sec')
+    plt.title('Rows fetched / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 5)
+    plt.plot(time,rows_inserted_per_sec, label='Rows inserted / sec')
+    plt.title('Rows Inserted / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 6)
+    plt.plot(time,rows_updated_per_sec, label='Rows updated / sec')
+    plt.title('Rows updated / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 7)
+    plt.plot(time,rows_deleted_per_sec, label='Rows deleted / sec')
+    plt.title('Rows deleted / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 8)
+    plt.plot(time,blocks_read_per_sec, label='Blocks read / sec')
+    plt.title('Blocks Inserted / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 9)
+    plt.plot(time,blocks_hit_cached_per_sec, label='Blocks hit cached / sec')
+    plt.title('Blocks hit cached / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 12)
+    plt.plot(time,txn_per_sec, label='transactions / sec')
+    plt.title('transactions / sec')
+    plt.xticks(rotation=45)
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.75,wspace=0.35) #####
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(30, 15)
+    plt.savefig("db_plot.png", dpi = 100)
+    #plt.show()
+    plt.gcf().clear()
+
+def plot_tablestatz(table_name):
+    tabledata = get_tablestatz(table_name)
+    values = zip(*tabledata)
+#
+    time = values[0]
+    seq_scans_per_sec = values[3]
+    seq_rows_read_per_sec = values[4]
+    index_scans_per_sec = values[5]
+    index_rows_fetched_per_sec = values[6]
+    rows_inserted_per_sec = values[7]
+    rows_updated_per_sec = values[8]
+    rows_deleted_per_sec = values[9]
+    rows_hot_updated_per_sec = values[10]
+#
+    tabletitle = """{0}""".format(table_name)
+    plt.suptitle('Table statistics / sec for ' + tabletitle, fontsize=28, fontweight='bold')
+    plt.subplot(3, 4, 1)
+    plt.plot(time,seq_scans_per_sec, label='Sequential scans / sec' )
+    plt.title('Sequential scans / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 2)
+    plt.plot(time,seq_rows_read_per_sec )
+    plt.title('Sequential rows read / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 3)
+    plt.plot(time,index_scans_per_sec )
+    plt.title('Index scans / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 4)
+    plt.plot(time,index_rows_fetched_per_sec, label='Rows fetched from index / sec')
+    plt.title('Rows fetched from index / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 5)
+    plt.plot(time,rows_inserted_per_sec, label='Rows inserted / sec')
+    plt.title('Rows inserted / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 6)
+    plt.plot(time,rows_updated_per_sec, label='Rows updated / sec')
+    plt.title('Rows updated / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 7)
+    plt.plot(time,rows_deleted_per_sec, label='Rows deleted / sec')
+    plt.title('Rows deleted / sec')
+    plt.xticks(rotation=45)
+    plt.subplot(3, 4, 8)
+    plt.plot(time,rows_hot_updated_per_sec, label='Rows hot updated / sec')
+    plt.title('Rows hot updated / sec')
+    plt.xticks(rotation=45)
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.75,wspace=0.35) #####
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(30, 15)
+
+    tablefile = tabletitle + ".png"
+    print tablefile
+    plt.savefig(tablefile, dpi = 100)
+    #plt.show()
+    plt.gcf().clear()
+
+def main():
+    tablez = []
+    tablez = args.table_to_graph
+    if args.do_db_graph is True:
+        plot_dbstatz()
+    if tablez:
+        for table in tablez:
+            plot_tablestatz(table)
+    else:
+        exit(0)
+#    if args.table_to_graph:
+#        print "oh noes"
+#    else:
+#        print (args.table_to_graph)
+
+if __name__ == "__main__":
+    main()
